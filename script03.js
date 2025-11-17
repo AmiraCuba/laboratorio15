d9fb361c95dbbacb3f626c7acbcbe5801a9ffefb
Amira <bcuba@unsa.edu.pe>
2025-11-17 16:52:35 -0500

    Ejercicio 03

---
+++ script03.js
@@ -0,0 +1,3 @@
+const imagen1 = document.querySelector("img");
+imagen1.setAttribute("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvoXBaizjWp_DhC3RwEPi_gs6757NjNA30ZQ&s")
+imagen1.setAttribute("alt", "Perro con una calabaza")
